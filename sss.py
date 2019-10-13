e21e7684543032826071c79af9fbe0d767293fcf
Misha <330110432@qq.com>
2019-10-13 20:57:56 +0800

    Create sss.py

---
+++ sss.py
@@ -0,0 +1,77 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/index", methods=["GET"])
+def index():
+    return "<h1>Hello World! I am phippy!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/index", methods=["GET"])
+def index():
+    return "<h1>Hello World! I am phippy!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/index", methods=["GET"])
+def index():
+    return "<h1>Hello World! I am phippy!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/index", methods=["GET"])
+def index():
+    return "<h1>Hello World! I am phippy!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/index", methods=["GET"])
+def index():
+    return "<h1>Hello World! I am phippy!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/index", methods=["GET"])
+def index():
+    return "<h1>Hello World! I am phippy!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/index", methods=["GET"])
+def index():
+    return "<h1>Hello World! I am phippy!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
