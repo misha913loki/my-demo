c2be380b91c5fed09cfce22dede8dfe6e5b1d939
Misha <330110432@qq.com>
2023-01-29 17:35:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,16 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World! I am phippy!</h1>"
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+  
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/index", methods=["GET"])
+def index():
+    return "<h1>Hello World! I am phippy!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)  
