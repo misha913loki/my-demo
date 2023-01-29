b538918c55a136fd1faefec600781e81d15da83d
Misha <330110432@qq.com>
2023-01-29 18:09:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)  
 from flask import Flask, jsonify
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  
+from flask import Flask, jsonify
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  
+from flask import Flask, jsonify
+
+
 app = Flask(__name__)
 
 @app.route("/index", methods=["GET"])
